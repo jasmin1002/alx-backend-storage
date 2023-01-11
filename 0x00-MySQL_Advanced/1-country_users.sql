536ddce2460f85b0a9b2cb5754533f3e847e6e6b
Jasmin1002 <jasmineolofin@gmail.com>
2023-01-11 22:12:40 +0100

    feat: Add script to create table with ENUM column

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,10 @@
+-- create table users
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE IF NOT EXISTS users (
+    id int NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL,
+    PRIMARY KEY (id)
+);
