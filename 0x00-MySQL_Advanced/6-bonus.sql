f7492e02ef968d668589901e1d29d509416c9e41
Jasmin1002 <jasmineolofin@gmail.com>
2023-01-15 01:29:51 +0100

    refactor: re-structure procedural code

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -8,16 +8,13 @@ CREATE PROCEDURE AddBonus (
 BEGIN
     SET @test := -1;
     SELECT id INTO @test FROM projects WHERE name = project_name;
-    -- select @test;
 
-    IF @test > 0 THEN
-        UPDATE corrections SET score = score
-        WHERE user_id = user_id AND project_id = @test;
-    ELSE
+    IF @test < 0 THEN
         INSERT INTO projects (name) VALUES (project_name);
-        INSERT INTO corrections (user_id, project_id, score)
-        VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
     END IF;
+    
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES (user_id, (SELECT id FROM projects WHERE name = project_name), score);
 END //
 
 DELIMITER ;
